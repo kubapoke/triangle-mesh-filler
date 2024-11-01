9aced7a4f849aa30b8d09581851ae7fcb1996efd
kubapoke <q.paczka@gmail.com>
2024-11-01 17:18:42 +0100

    added UI sliders

--- TriangleFilling/MainForm.Designer.cs
+++ TriangleFilling/MainForm.Designer.cs
@@ -38,6 +38,16 @@
             alphaDegreeTrackBar = new TrackBar();
             label1 = new Label();
             precisionTrackBar = new TrackBar();
+            label4 = new Label();
+            mTrackBar = new TrackBar();
+            label5 = new Label();
+            ksTrackBar = new TrackBar();
+            label6 = new Label();
+            kdTrackBar = new TrackBar();
+            label7 = new Label();
+            lightRotationTrackBar = new TrackBar();
+            label8 = new Label();
+            lightHeightTrackBar = new TrackBar();
             ((System.ComponentModel.ISupportInitialize)mainSplitContainer).BeginInit();
             mainSplitContainer.Panel1.SuspendLayout();
             mainSplitContainer.Panel2.SuspendLayout();
@@ -46,6 +56,11 @@
             ((System.ComponentModel.ISupportInitialize)betaDegreeTrackBar).BeginInit();
             ((System.ComponentModel.ISupportInitialize)alphaDegreeTrackBar).BeginInit();
             ((System.ComponentModel.ISupportInitialize)precisionTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ksTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kdTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)lightRotationTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)lightHeightTrackBar).BeginInit();
             SuspendLayout();
             // 
             // mainSplitContainer
@@ -62,6 +77,16 @@
             // 
             // mainSplitContainer.Panel2
             // 
+            mainSplitContainer.Panel2.Controls.Add(label7);
+            mainSplitContainer.Panel2.Controls.Add(lightRotationTrackBar);
+            mainSplitContainer.Panel2.Controls.Add(label8);
+            mainSplitContainer.Panel2.Controls.Add(lightHeightTrackBar);
+            mainSplitContainer.Panel2.Controls.Add(label4);
+            mainSplitContainer.Panel2.Controls.Add(mTrackBar);
+            mainSplitContainer.Panel2.Controls.Add(label5);
+            mainSplitContainer.Panel2.Controls.Add(ksTrackBar);
+            mainSplitContainer.Panel2.Controls.Add(label6);
+            mainSplitContainer.Panel2.Controls.Add(kdTrackBar);
             mainSplitContainer.Panel2.Controls.Add(fillCheckBox);
             mainSplitContainer.Panel2.Controls.Add(outlineCheckbox);
             mainSplitContainer.Panel2.Controls.Add(label3);
@@ -70,7 +95,8 @@
             mainSplitContainer.Panel2.Controls.Add(alphaDegreeTrackBar);
             mainSplitContainer.Panel2.Controls.Add(label1);
             mainSplitContainer.Panel2.Controls.Add(precisionTrackBar);
-            mainSplitContainer.Size = new Size(800, 450);
+            mainSplitContainer.Panel2.Paint += this.mainSplitContainer_Panel2_Paint;
+            mainSplitContainer.Size = new Size(800, 521);
             mainSplitContainer.SplitterDistance = 550;
             mainSplitContainer.TabIndex = 0;
             // 
@@ -79,7 +105,7 @@
             mainPictureBox.Dock = DockStyle.Fill;
             mainPictureBox.Location = new Point(0, 0);
             mainPictureBox.Name = "mainPictureBox";
-            mainPictureBox.Size = new Size(550, 450);
+            mainPictureBox.Size = new Size(550, 521);
             mainPictureBox.TabIndex = 0;
             mainPictureBox.TabStop = false;
             mainPictureBox.Paint += mainPictureBox_Paint;
@@ -89,7 +115,7 @@
             fillCheckBox.AutoSize = true;
             fillCheckBox.Checked = true;
             fillCheckBox.CheckState = CheckState.Checked;
-            fillCheckBox.Location = new Point(144, 165);
+            fillCheckBox.Location = new Point(144, 420);
             fillCheckBox.Name = "fillCheckBox";
             fillCheckBox.Size = new Size(70, 19);
             fillCheckBox.TabIndex = 7;
@@ -102,7 +128,7 @@
             outlineCheckbox.AutoSize = true;
             outlineCheckbox.Checked = true;
             outlineCheckbox.CheckState = CheckState.Checked;
-            outlineCheckbox.Location = new Point(21, 165);
+            outlineCheckbox.Location = new Point(21, 420);
             outlineCheckbox.Name = "outlineCheckbox";
             outlineCheckbox.Size = new Size(94, 19);
             outlineCheckbox.TabIndex = 6;
@@ -169,13 +195,109 @@
             precisionTrackBar.Value = 16;
             precisionTrackBar.Scroll += precisionTrackBar_Scroll;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(10, 270);
+            label4.Name = "label4";
+            label4.Size = new Size(21, 15);
+            label4.TabIndex = 13;
+            label4.Text = "m:";
+            // 
+            // mTrackBar
+            // 
+            mTrackBar.Location = new Point(69, 267);
+            mTrackBar.Maximum = 100;
+            mTrackBar.Minimum = 1;
+            mTrackBar.Name = "mTrackBar";
+            mTrackBar.Size = new Size(174, 45);
+            mTrackBar.TabIndex = 12;
+            mTrackBar.TickFrequency = 10;
+            mTrackBar.Value = 50;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(10, 219);
+            label5.Name = "label5";
+            label5.Size = new Size(21, 15);
+            label5.TabIndex = 11;
+            label5.Text = "ks:";
+            // 
+            // ksTrackBar
+            // 
+            ksTrackBar.Location = new Point(69, 216);
+            ksTrackBar.Maximum = 100;
+            ksTrackBar.Name = "ksTrackBar";
+            ksTrackBar.Size = new Size(174, 45);
+            ksTrackBar.TabIndex = 10;
+            ksTrackBar.TickFrequency = 9;
+            ksTrackBar.Value = 50;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(10, 168);
+            label6.Name = "label6";
+            label6.Size = new Size(23, 15);
+            label6.TabIndex = 9;
+            label6.Text = "kd:";
+            // 
+            // kdTrackBar
+            // 
+            kdTrackBar.Location = new Point(69, 165);
+            kdTrackBar.Maximum = 100;
+            kdTrackBar.Name = "kdTrackBar";
+            kdTrackBar.Size = new Size(174, 45);
+            kdTrackBar.TabIndex = 8;
+            kdTrackBar.TickFrequency = 10;
+            kdTrackBar.Value = 50;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(10, 372);
+            label7.Name = "label7";
+            label7.Size = new Size(52, 15);
+            label7.TabIndex = 17;
+            label7.Text = "light rot:";
+            // 
+            // lightRotationTrackBar
+            // 
+            lightRotationTrackBar.Location = new Point(69, 369);
+            lightRotationTrackBar.Maximum = 200;
+            lightRotationTrackBar.Name = "lightRotationTrackBar";
+            lightRotationTrackBar.Size = new Size(174, 45);
+            lightRotationTrackBar.TabIndex = 16;
+            lightRotationTrackBar.TickFrequency = 20;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(10, 321);
+            label8.Name = "label8";
+            label8.Size = new Size(48, 15);
+            label8.TabIndex = 15;
+            label8.Text = "light ht:";
+            // 
+            // lightHeightTrackBar
+            // 
+            lightHeightTrackBar.Location = new Point(69, 318);
+            lightHeightTrackBar.Maximum = 1000;
+            lightHeightTrackBar.Minimum = 300;
+            lightHeightTrackBar.Name = "lightHeightTrackBar";
+            lightHeightTrackBar.Size = new Size(174, 45);
+            lightHeightTrackBar.TabIndex = 14;
+            lightHeightTrackBar.TickFrequency = 70;
+            lightHeightTrackBar.Value = 500;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 521);
             Controls.Add(mainSplitContainer);
-            MinimumSize = new Size(816, 489);
+            MinimumSize = new Size(816, 560);
             Name = "MainForm";
             Text = "Triangle Filler";
             mainSplitContainer.Panel1.ResumeLayout(false);
@@ -187,6 +309,11 @@
             ((System.ComponentModel.ISupportInitialize)betaDegreeTrackBar).EndInit();
             ((System.ComponentModel.ISupportInitialize)alphaDegreeTrackBar).EndInit();
             ((System.ComponentModel.ISupportInitialize)precisionTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ksTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kdTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)lightRotationTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)lightHeightTrackBar).EndInit();
             ResumeLayout(false);
         }
 
@@ -202,5 +329,15 @@
         private TrackBar alphaDegreeTrackBar;
         private CheckBox outlineCheckbox;
         private CheckBox fillCheckBox;
+        private Label label4;
+        private TrackBar mTrackBar;
+        private Label label5;
+        private TrackBar ksTrackBar;
+        private Label label6;
+        private TrackBar kdTrackBar;
+        private Label label7;
+        private TrackBar lightRotationTrackBar;
+        private Label label8;
+        private TrackBar lightHeightTrackBar;
     }
 }
