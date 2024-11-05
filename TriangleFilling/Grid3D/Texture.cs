1d2fb88b5a9024ba322cf8ed9463672aa78e6e06
kubapoke <q.paczka@gmail.com>
2024-11-06 00:09:54 +0100

    implemented texture class

---
+++ TriangleFilling/Grid3D/Texture.cs
@@ -0,0 +1,22 @@
+﻿namespace TriangleFilling.Grid3D
+{
+    internal class Texture
+    {
+        public Bitmap Bitmap;
+        private int Width, Height;
+
+        public Texture(string imagePath)
+        {
+            Bitmap = (Bitmap)Image.FromFile(imagePath);
+            Width = Bitmap.Width;
+            Height = Bitmap.Height;
+        }
+
+        public Color GetPixel(float u, float v)
+        {
+            int x = (int)(Width * u);
+            int y = (int)(Height * v);
+            return Bitmap.GetPixel(x, y);
+        }
+    }
+}
